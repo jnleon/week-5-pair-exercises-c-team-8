5dcf927e38c1c52fd84ca8ad9eb574ea3a9555c2
Juan Leon <juanix131@hotmail.com>
2020-10-14 16:37:59 -0400

    PAIR 03-JOIN HW FINISH

--- 03_Joins/pair-exercise/joins-exercises.sql
+++ 03_Joins/pair-exercise/joins-exercises.sql
@@ -39,14 +39,44 @@ WHERE co.continent = 'Europe'
 -- 6. The average city population of cities in Asia
 -- (average city population in Asia: 395,019)
 
+SELECT AVG(ci.population) AS 'Average Population'
+FROM city ci
+JOIN country co ON ci.countrycode = co.code
+WHERE co.continent = 'Asia'
+
 -- 7. The number of cities in countries where English is an official language
 -- (number of cities where English is official language: 523)
+
+SELECT COUNT(ci.name) AS 'number of cities where English is official language'
+FROM city ci
+JOIN country co ON ci.countrycode = co.code
+JOIN countrylanguage cl ON co.code = cl.countrycode
+WHERE cl.language ='English' AND cl.isofficial= 1;
 
 -- 8. The average population of cities in countries where the official language is English
 -- (average population of cities where English is official language: 285,809)
 
+SELECT AVG(ci.population) AS 'average population of cities where English is official language'
+FROM city ci
+JOIN country co ON ci.countrycode = co.code
+JOIN countrylanguage cl ON co.code = cl.countrycode
+WHERE cl.language ='English' AND cl.isofficial= 1;
+
 -- 9. The names of all of the continents and the population of the continent’s largest city
 -- (6 rows, largest population for North America: 8,591,309)
 
+SELECT co.continent, MAX(ci.population) AS 'population largest city'
+FROM country co
+JOIN city ci ON ci.countrycode = co.code
+GROUP BY co.continent
+ORDER BY 'population largest city' desc;
+
+
 -- 10. The names of all of the cities in South America that have a population of more than 1 million people and the official language of each city’s country
 -- (29 rows)
+
+SELECT ci.name , cl.language
+FROM city ci
+JOIN country co ON ci.countrycode = co.code
+JOIN countrylanguage cl ON co.code = cl.countrycode
+WHERE co.continent = 'South America' AND ci.population > 1000000 AND cl.isofficial = 1;
