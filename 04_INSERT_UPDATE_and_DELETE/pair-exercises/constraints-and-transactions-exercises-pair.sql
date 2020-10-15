0283576810b0f1ddb5165de46d26eb1279769fe3
Zachary Wilson <zlwilson21@gmail.com>
2020-10-15 13:36:11 -0400

    Pair exercises 04 DML complete

--- 04_INSERT_UPDATE_and_DELETE/pair-exercises/constraints-and-transactions-exercises-pair.sql
+++ 04_INSERT_UPDATE_and_DELETE/pair-exercises/constraints-and-transactions-exercises-pair.sql
@@ -40,19 +40,58 @@ WHERE id= (select id from city WHERE name = 'Smallville')
 --*******IT FAILED, WE ENCOUNTERED A CONSTRAINT WHEN TRYING TO DELETE IT BECAUSE ITS SET TO THE CAPITAL IT CANT BE DELETED.
 
 -- 6. Return the US captial to Washington.
+UPDATE country
+SET capital = (select id from city WHERE name = 'Washington')
+WHERE CODE = 'USA'
+
 
 -- 7. Delete Smallville, Kansas from the city table. (Did it succeed? Why?)
+DELETE
+--SELECT *
+FROM city
+WHERE id= (select id from city WHERE name = 'Smallville')
+
+--IT WAS SUCCESSFUL. THERE IS NO LONGER A CONFLICT WITH THE CAPITAL CONSTRAINT.
 
 -- 8. Reverse the "is the official language" setting for all languages where the
 -- country's year of independence is within the range of 1800 and 1972 
 -- (exclusive). 
 -- (590 rows affected)
+BEGIN TRANSACTION
+
+--SELECT * FROM countrylanguage WHERE countrycode IN (SELECT code FROM country WHERE indepyear >= 1800 AND indepyear <= 1972);
+
+Update countrylanguage 
+SET isofficial = ~isofficial 
+WHERE countrycode IN (SELECT code FROM country WHERE indepyear >= 1800 AND indepyear <= 1972);
+
+--SELECT * FROM countrylanguage WHERE countrycode IN (SELECT code FROM country WHERE indepyear >= 1800 AND indepyear <= 1972);
+
+COMMIT TRANSACTION
 
 -- 9. Convert population so it is expressed in 1,000s for all cities. (Round to
 -- the nearest integer value greater than 0.)
 -- (4079 rows affected)
 
+BEGIN TRANSACTION
+--SELECT population, name FROM city
+UPDATE city
+SET population = ROUND(population/1000, 0)
+--SELECT population, name FROM city
+COMMIT TRANSACTION
+
 -- 10. Assuming a country's surfacearea is expressed in square miles, convert it to 
 -- square meters for all countries where French is spoken by more than 20% of the 
 -- population.
 -- (7 rows affected)
+BEGIN TRANSACTION
+
+--SELECT * FROM  COUNTRY WHERE code IN (SELECT countrycode FROM countrylanguage WHERE language = 'French' AND percentage > 20)
+
+UPDATE country
+SET surfacearea = (surfacearea*2589988.1)
+WHERE code IN (SELECT countrycode FROM countrylanguage WHERE language = 'French' AND percentage > 20)
+
+--SELECT * FROM  COUNTRY WHERE code IN (SELECT countrycode FROM countrylanguage WHERE language = 'French' AND percentage > 20)
+
+COMMIT TRANSACTION
